d25309987bb3f9d24749f36c0cf0881f64bef518
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-15 10:57:18 +0530

    adding daily wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,15 +2,25 @@ public class EmployeeWage
 {
 	public static void main(String[] args)
 	{
+		int empHrs=0;
+		int empWage=0;
+		int wagePerHour=20;
+
 		System.out.println("Welcome to employee wage problem");
 
 int fullTime=1;
 		int randomCheck=(int) (Math.random()*10)%2;
 		if(randomCheck==fullTime)
+		{
 			System.out.println("Employee is present");
+			empHrs=8;
+			}
 		else
+		{	
 			System.out.println("Employee is absent");
-
-
+				empHrs=0;
+			}
+			empWage=wagePerHour*empHrs;
+		System.out.println("Employee daily wage:" + empWage);
 }
 }
