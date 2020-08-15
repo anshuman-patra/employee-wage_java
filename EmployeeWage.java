c07fdaa8125023ee0e0e46821bb6878364dddfd3
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-15 11:44:33 +0530

    monthly wage of employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,16 @@
 public class EmployeeWage
-{
+{ public static final int EMP_RATE_PER_HOUR=20;
+                public static final int NUMBER_WORKING_DAYS=20;
+
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to employee wage problem");
-
+		int  totalSalary=0;
 		int empHrs=0;
                 int empWage=0;
                 int wagePerHour=20;
+		for(int i=0; i<NUMBER_WORKING_DAYS; i++)
+		{ 
 		int randomCheck=(int) (Math.random()*10)%3;
 		switch(randomCheck)
 		     {
@@ -29,7 +33,11 @@ public class EmployeeWage
 				empHrs=0;
 				break;
 			}
-			empWage=wagePerHour*empHrs;
-		System.out.println("Employee daily wage:" + empWage);
+
+			empWage=EMP_RATE_PER_HOUR*empHrs;
+			System.out.println("Employee daily wage:" + empWage);
+			totalSalary=totalSalary+empWage;
+		}
+		System.out.println("Total employee wage:" + totalSalary);
 	}
 }
